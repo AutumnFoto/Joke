7d2f4ef8fe4e38a7ade4b6ac1b8ad4957696aab9
Autumn Fotopoulos <acarte94@gmail.com>
2020-10-31 13:55:53 -0500

    joke generator

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,30 @@
 import '../styles/main.scss';
+import axios from 'axios';
+
+const getJoke = () => new Promise((resolve, reject) => {
+  axios.get('https://official-joke-api.appspot.com/random_joke').then((response) => {
+    const thisJoke = response.data;
+    resolve(thisJoke);
+  }).catch((error) => reject(error));
+});
+
+$('body').on('click', '#joke-btn', (e) => {
+  e.stopImmediatePropagation();
+  getJoke().then((response) => {
+    const data = response;
+    $('#joke-display').html(data.setup);
+    $('#punchline-display').hide();
+    $('#punchline-display').html(data.punchline);
+  });
+});
+
+$('body').on('click', '#punchline-btn', (e) => {
+  e.stopImmediatePropagation();
+  $('#punchline-display').show();
+});
 
 const init = () => {
-  $('#app').html('<h1>HELLO! You are up and running!</h1>');
+  $('#app').html("<div id = 'joke-div'><a href='#' id='joke-btn' class='btn btn-primary'>Get Joke</a></div><div id= 'punchline-div'></div> <a href='#' id='punchline-btn' class='btn btn-primary'>Punchline</a><div id = 'joke-display'></div><div id= 'punchline-display'></div>");
   console.log('YOU ARE UP AND RUNNING!');
 };
 
